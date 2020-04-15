6be47592f73357095e3b0ee9dbf60d3406295d83
Yan Chen <cyanashton@gmail.com>
2020-04-15 15:12:31 -0400

    started OOP

--- ProjectMayhem/src/com/revature/ol/Calculator.java
+++ ProjectMayhem/src/com/revature/ol/Calculator.java
@@ -6,6 +6,7 @@ public class Calculator {
 		//TODO Auto-generated mehod stub
 		add(3,6);
 		add(1.3, 4.5);
+		add(4,5,6);
 	}
 	
 	static int add(int a, int b) {
@@ -17,6 +18,10 @@ public class Calculator {
 	static double add(double a, double b) {
 		System.out.println(a+b);
 		return a+b;
+	}
+	
+	static void add (int a, int b, int c) {
+		System.out.println(a+b+c);
 	}
 
 }
