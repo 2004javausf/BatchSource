527a75bcddd4048a63aaac9415463f19f24d7e54
Matt Knighten <jason.knighten@revature.com>
2020-04-16 12:13:29 -0400

    push just in case

--- ProjectMayhem/src/com/revature/ol/Calculator.java
+++ ProjectMayhem/src/com/revature/ol/Calculator.java
@@ -1,8 +1,17 @@
 package com.revature.ol;
 
 public class Calculator {
-
+	
+	static int a;
+	static double b;
+	
 	public static void main(String[] args) {
+		a=5;
+		b=a;	
+		b=3.9;
+		a=(int)b;
+		System.out.println(a);
+		System.out.println(b);
 		add(add(5,4),6);
 		add(1.3, 4.5);
 		add(4,5,6);
