b0bd6f01c791bc248da276e3c9aec77a5a0f1c20
Jason W. Clary <jwclary411@gmail.com>
2020-06-17 10:41:36 -0400

    update

---
+++ user-service/bin/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO app_users (username, password, first_name, last_name)
+VALUES ('wsingleton', 'password', 'Wezley', 'Singleton');
+
+INSERT INTO app_users (username, password, first_name, last_name)
+VALUES ('jknighten', 'rolltide', 'Jason', 'Knighten');
